7fbe833de8bd9e0e13c45cf01c58072f1ec1c042
BEKZAD SARYBAEV <bekzadsarybaev@MacBook-Pro-BEKZAD.local>
2022-08-25 00:01:09 +0600

    Initial commit

---
+++ src/People.java
@@ -0,0 +1,13 @@
+public abstract class Person {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String sity;
+
+    public abstract void work();
+    public abstract void ToRelax();
+    public abstract void comunications();
+    public abstract void timetable();
+}
